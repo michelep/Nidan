2c94fa0506177f1dea32393a58d8b69bb20b8537
O-Zone <o-zone@zerozone.it>
2017-09-13 16:02:01 +0200

    fixes and improvements

---
+++ web/config.php
@@ -0,0 +1,51 @@
+<?php 
+
+include_once "common.inc.php";
+
+if(!$mySession->isLogged()) {
+    header("Location: /signin.php");
+    exit();
+}
+
+$pageTitle = "System configuration";
+
+include "common_head.php"; 
+
+include_once "common_sidebar.php";
+
+?>
+<main class="col-sm-9 offset-sm-3 col-md-10 offset-md-2 pt-3" id="contentDiv">
+    <h2>System Configuration</h2>
+    <div class="table-responsive">
+	<table class="table table-striped"><thead>
+	    <tr>
+    		<th>Name</th>
+		<th>Value</th>
+		<th></th>
+	    </tr>
+	</thead><tbody>
+<?php
+	$result = doQuery("SELECT Name, Value FROM Config ORDER BY Name;");
+	if(mysqli_num_rows($result) > 0) {
+	    while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+	        $config_name = stripslashes($row["Name"]);
+		$config_value = stripslashes($row["Value"]);
+		echo "<tr>
+		    <td>$config_name</td>
+		    <td>".htmlspecialchars($config_value)."</td>
+		    <td>
+			<a class='ajaxDialog' title='Edit field' href='/ajax?action=config_edit&name=$config_name'><i class='fa fa-pencil-square' aria-hidden='true'></i></a>
+		    </td>
+		</tr>";
+	    }
+	}
+?>
+	</tbody></table>
+    </div>
+</main>
+
+<?php
+
+include "common_foot.php";
+
+?>
---
+++ web/install/index.php
@@ -0,0 +1,124 @@
+<?php
+
+include "../config.inc.php";
+
+function check_db($host,$port,$user,$password) {
+    $mysqli = mysqli_init();
+    if (!$mysqli) {
+        return "mysqli_init() failed !";
+    }
+
+    if (!$mysqli->real_connect($host, $user, $password)) {
+        return "Connect Error (".mysqli_connect_errno().") ".mysqli_connect_error();
+    }
+
+    $mysqli->close();
+    return false;
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="Nidan">
+    <meta name="author" content="Michele 'O-Zone' Pinassi">
+    <link rel="icon" href="/favicon.ico">
+    <link href="/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/css/tether.min.css" rel="stylesheet">
+    <link href="/css/font-awesome.min.css" rel="stylesheet">
+    <link href="/css/bootstrap-table.min.css" rel="stylesheet">
+    <link href="/css/validationEngine.jquery.css" rel="stylesheet">
+    <link href="/css/jquery-ui.min.css" rel="stylesheet">
+    <link href="/css/noty.css" rel="stylesheet">
+    <link href="/css/common.css" rel="stylesheet">
+
+    <title>Nidan installer</title>
+</head><body>
+    <nav class="navbar navbar-toggleable-md navbar-inverse fixed-top bg-inverse">
+	<button class="navbar-toggler navbar-toggler-right hidden-lg-up" type="button" data-toggle="collapse" data-target="#navbarTop" aria-controls="navbarTop" aria-expanded="false" aria-label="Toggle navigation">
+	    <span class="navbar-toggler-icon"></span>
+	</button>
+	<a class="navbar-brand" href="/"><img src="/img/header_logo.png" class="img-responsive header-logo" /> Nidan installer</a>
+    </nav>
+    <div class="container-fluid"><!-- CONTAINER -->
+	<div class="row"><!-- ROW -->
+	    <main class="col-sm-8 py-4 offset-sm-2" id="contentDiv">
+<?php
+if(isset($_POST["step"])) {
+    $step = intval($_POST["step"]);
+    if($step == 1) { // Check database connectivity
+	$db_host = $_POST["db_host"];
+	$db_port = $_POST["db_port"];
+	$db_user = $_POST["db_user"];
+	$db_password = $_POST["db_password"];
+
+	$res = check_db($db_host,$db_port,$db_user,$db_password);
+	if($res != false) {
+	    echo "<div class='alert alert-warning'>
+		$res;
+	    </div>";
+	    $step = 0;
+	} else {
+	    echo "<div class='alert alert-success'>
+		<strong>Well done!</strong> Successfully connected to the DB 
+	    </div>";
+	}
+    
+
+    }
+} 
+
+if($step == 0) {// First step
+?>
+		<p class="h1">1. Check connection with DBMS</p>
+		<form method="POST">
+		    <input type="hidden" name="step" value="1">
+		    <div class="form-group">
+			<label for="dbHost">Database hostname</label>
+			<input type="text" class="form-control validate[required]" id="dbHost" name="db_host" placeholder="Hostname or IP of your DBMS (i.e. localhost)" value="<?php echo $CFG["db_host"]; ?>">
+		    </div><div class="form-group">
+			<label for="dbPort">Database port</label>
+			<input type="text" class="form-control validate[required]" id="dbPort" name="db_port" placeholder="Port of your DBMS (i.e. 3306)" value="<?php echo $CFG["db_port"]; ?>">
+		    </div><div class="form-group">
+			<label for="dbUser">Database user</label>
+			<input type="text" class="form-control validate[required]" id="dbUser" name="db_user" placeholder="DBMS user (i.e. root)" value="<?php echo $CFG["db_user"]; ?>">
+		    </div><div class="form-group">
+			<label for="dbPassword">Database password</label>
+			<input type="password" class="form-control validate[required]" id="dbPassword" name="db_password" placeholder="DBMS password" value="<?php echo $CFG["db_password"]; ?>">
+		    </div><div class="form-group">
+			<input type="submit" value="Check database">
+		    </div>
+		</form>
+<?php
+}
+?>
+	    </main>
+	</div><!-- /ROW -->
+    </div><!-- /CONTAINER -->
+    <footer class="footer">
+        <div class="container">
+	    <div class="row justify-content-md-center">
+    	        <div class="text-center">
+            	    <h4>
+			<strong>Nidan</strong>
+        	    </h4>
+        	    <p>Made with <i class="fa fa-heart fa-fw"></i> in Siena, Tuscany, Italy</p>
+		    <p>by O-Zone &lt;<a href="mailto:o-zone@zerozone.it">o-zone@zerozone.it</a>&gt;</p>
+        	</div>
+    	    </div>
+	</div>
+    </footer>
+    <script src="/js/jquery.min.js"></script>
+    <script src="/js/jquery-ui.min.js"></script>
+    <script>window.jQuery || document.write('<script src="/js/jquery.min.js"><\/script>')</script>
+    <script src="/js/tether.min.js"></script>
+    <script src="/js/bootstrap.min.js"></script>
+    <!-- IE10 viewport hack for Surface/desktop Windows 8 bug -->
+    <script src="/js/ie10-viewport-bug-workaround.js"></script>
+    <script src="/js/bootstrap-table.min.js"></script>
+    <script src="/js/jquery.validationEngine-en.js"></script>
+    <script src="/js/jquery.validationEngine.js"></script>
+    <script src="/js/common.js"></script>
+</body></html>
